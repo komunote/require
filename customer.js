064c2da1d94aa6bf0daec2a6fb014c71bbf96741
David Chabrier <david_chabrier@hotmail.com>
2016-06-10 14:53:38 -0400

    update customer.js
    attach method added
    attach a method into the window object

--- customer.js
+++ customer.js
@@ -1,52 +1,51 @@
-window.customerButtonClick = function() {		
+r.attach('customerButtonClick', function() {
 	alert('button clicked');
 	console.log(this);
 	
-	require(["customer", "text!customer.html"], function(customer, customerTpl){
+	require(["customer", "text!customer.html"], (customer, customerTpl) =>
+	{
 		r.get('/user/1/')
 		.then(function(data) {			
 			var html = template(customerTpl, data);
 			console.log('---------------customer.js loaded----------------');	
-			console.log(html);
-			console.log(r.append(html, document.body));			
+			//console.log(html);			
+			r.append(html, document.body);
 		})
 		.catch(function(error){
 			console.log(error);
-		});
+		});		
 	});
-};
+});
 
-window.customerTextChange = function() {
+r.attach('customerTextChange', function() {
 	alert('text changed');
 	console.log(this);
-}
+});
 
-define(["text!util.html"], function (tpl) {
-
-	
-	
-	return {
-		utils : function() 
-		{
+define(["text!customer.html"], tpl => (
+	{
+		getView : function() {
 			return template(tpl, 
 				{
-					"firstname":"David--",
-					"lastname":"Chabrier--",
-					"for-customers":
-					[
-						{
-							"id":1,
-							"lastname":"Chabrier 1-",
-							"firstname":"David 1-"
-						},
-						{
-							"firstname":"David 2-",
-							"lastname":"Chabrier 2-",
-							"id":2
-						}
-					]
+					"identity":{
+						"firstname":"David--",
+						"lastname":"Chabrier--",
+						"customers":
+						[
+							{
+								"id":1,
+								"lastname":"Chabrier 1-",
+								"firstname":"David 1-"
+							},
+							{
+								"firstname":"David 2-",
+								"lastname":"Chabrier 2-",
+								"id":2
+							}
+						]
+					}					
 				}
 			);
 		}
-	}
-});
+	})		
+);
